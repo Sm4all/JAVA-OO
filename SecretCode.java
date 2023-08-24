6171a6fe5a230367bb4ff5e225ec7defbe88f3c3
Sm4all <134271104+Sm4all@users.noreply.github.com>
2023-08-24 15:24:35 +0100

    Added files

---
+++ SecretCode.java
@@ -0,0 +1,88 @@
+package com.uni.pt222.backend;
+
+import java.util.Random;
+
+public class SecretCode {
+    //OBJECTS
+    private String secretCode;
+    private String fragmentOne;
+    private String fragmentTwo;
+    private String fragmentThree;
+    private String fragmentFour; //May be deleted
+    private String fragmentFive; //May be deleted
+
+    //METHOD
+    public SecretCode (String secretCode) {  
+        this.setSecretCode(secretCode);
+        this.setFragmentOne(secretCode);
+        this.setFragmentTwo(secretCode);
+        this.setFragmentThree(secretCode);
+        this.setFragmentFour(secretCode);
+        this.setFragmentFive(secretCode);
+    }
+
+    //Setters
+    public void setSecretCode(String secretCode) {
+        this.secretCode = secretCode;
+        secretCode = "";
+        for (int i=0; i < 15; i++) {
+                Random r = new Random();
+                secretCode = secretCode + (char)(r.nextInt(26) + 'A') + "-";
+            }
+    }
+
+    public void setFragmentOne(String fragmentOne) {
+        this.fragmentOne = fragmentOne;
+        String[] parts = secretCode.split("-");
+        fragmentOne = parts[0];
+    }
+    
+    public void setFragmentTwo(String fragmementTwo) {
+        this.fragmentTwo = fragmementTwo;
+        String[] parts = secretCode.split("-");
+        fragmementTwo = parts[1];
+    }
+
+    public void setFragmentThree(String fragmentThree) {
+        this.fragmentThree = fragmentThree;
+        String[] parts = secretCode.split("-");
+        fragmentThree = parts[2];
+    }
+
+    public void setFragmentFour(String fragmentFour) {
+        this.fragmentFour = fragmentFour;
+        String[] parts = secretCode.split("-");
+        fragmentFour = parts[3];
+    }
+
+    public void setFragmentFive(String fragmentFive) {
+        this.fragmentFive = fragmentFive;
+        String[] parts = secretCode.split("-");
+        fragmentFive = parts[4];
+    }
+
+    //GETTERS
+    public String getSecretCode() {
+        return secretCode;
+    }
+
+    public String getFragmentOne() {
+        return fragmentOne;
+    }
+
+    public String getFragmentTwo() {
+        return fragmentTwo;
+    }
+
+    public String getFragmentThree() {
+        return fragmentThree;
+    }
+
+    public String getFragmentFour() {
+        return fragmentFour;
+    }
+
+    public String getFragmentFive() {
+        return fragmentFive;
+    }
+}
---
+++ ideas+maze.md
@@ -0,0 +1,18 @@
+Ideas for Dungeon Escape + Maze map
+
+-4 by 4 grid
+-different things happening in each room
+    -Random chance of being led to the monster room and then redirected back
+    -Will have a health bar
+    -Some rooms will have traps and will make you lose health
+    -You have a monster chasing you and if you make a wrong move it will catch you up
+        -Say something like in this room you tripped and fell...The monster moved closer
+    -Rooms where you need to gather fragments of a code in the background image?
+    -You find a room at the end of the maze that requires a key to unlock the final door
+    -You must find the key to unlock the door somewhere else in the maze
+    -The key is protected by a code in which you must type in links to the code that is in line 10 of this.
+        -This forces the user to explore around the maze.
+        -Code randomly generates?
+            -Each fragment as different objects.
+-NPC encounter giving you advice and where to go after so many moves have been made
+-
